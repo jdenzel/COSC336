da2bc04224103df018ebf3bb9ab04efc5d5089f7
Denzel <jdenzelmab@gmail.com>
2024-11-26 20:42:42 -0500

    Completed Assignment 8

---
+++ Assignment-8/src/Adj_List_Graph2.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.io.*;
+
+  public class Adj_List_Graph2{
+    int n;  // no of nodes
+    ArrayList<ArrayList<Integer> > adj; 
+    //constructor taking as the single parameter the number of nodes
+    Adj_List_Graph2(int no_nodes) {
+      n = no_nodes;
+      adj =  new ArrayList<ArrayList<Integer> >(n);
+      for (int i = 0; i < n; i++) {
+        adj.add(new ArrayList<Integer>());
+      }
+    }
+
+    void addEdgeG1() {
+      addEdge(1, 2);
+      addEdge(1, 4);
+      addEdge(1, 3);
+      addEdge(2, 5);
+      addEdge(3, 5);
+      addEdge(4, 6);
+      addEdge(5, 6);
+      addEdge(5, 7);
+      addEdge(6, 7);
+    }
+
+    void addEdgeG2() {
+      addEdge(1, 2);
+      addEdge(1, 3);
+      addEdge(1, 4);
+      addEdge(1, 5);
+      addEdge(1, 6);
+      addEdge(2, 7);
+      addEdge(3, 7);
+      addEdge(4, 7);
+      addEdge(5, 7);
+      addEdge(6, 7);
+      addEdge(7, 8);
+      addEdge(7, 9);
+      addEdge(8, 10);
+      addEdge(9, 10);
+    }
+
+    public void shortestPath(int s, int v) {
+      // BFS for finding shortest path from s to v
+      Queue<Integer> q = new LinkedList<>(); // intialize queue
+      q.add(s); // starting node
+      int[] dist = new int[n]; // initialze array to track distance of starting node to the current node
+      int[] npath = new int[n]; // initialize array to track number of shortest paths based on current node
+      for (int i = 1; i < n; i++) {
+        dist[i] = -1; // sets unseen nodes to -1. Different from bool[] to track distance
+        npath[i] = 0; // sets all paths for each node to 0
+      }
+      dist[s] = 0; // sets distance of self node u to 0
+      npath[s] = 1; // sets starting node number of paths to 1
+
+      while (!q.isEmpty()) { // iterate while queue is not empty
+        int current = q.poll(); // poll() dequeues the node
+        for (int adjNode : adj.get(current)) {
+          if (dist[adjNode] == -1) { // if node is unseen
+            dist[adjNode] = dist[current] + 1; // set distance of adjacent node to distance of current node + 1 to indicate it has been seen
+            npath[adjNode] = npath[current]; // since the adjacent node has not been seen, it will be set to number of paths of current
+            q.add(adjNode); // adds adjacent node to the queue
+          }
+          else if (dist[adjNode] == dist[current] + 1) { 
+            npath[adjNode] += npath[current]; // since adjacent node has been seen, add number of paths of adjacent nodes to number of paths of current node
+          }
+        }
+      }
+      System.out.println("\ndist[" + v + "]" + " = " + dist[v]);
+      System.out.println("npath[" + v + "]" + " = " + npath[v]);
+      System.out.println("Array of distances from start node " + Arrays.toString(dist));
+      System.out.println("Array of shortest paths from start node " + Arrays.toString(npath));
+    }
+
+    // A utility function to add an edge in an
+    // undirected graph; for directed graph remove the second line
+    public void addEdge(int u, int v)
+    {
+      adj.get(u).add(v);
+      adj.get(v).add(u);  //this line should be un-commented, if graph is undirected
+    }
+    
+    public void printGraph()
+    {
+      for (int i = 1; i < n; i++) {
+        System.out.println("\nAdjacency list of vertex " + i);
+        System.out.print(i);
+        // System.out.print("head");
+        for (int j = 0; j < adj.get(i).size(); j++) {
+          System.out.print(" - "+adj.get(i).get(j));
+        }
+        System.out.println();
+      }
+    }
+  }
+
+
+  
+  
---
+++ Assignment-8/src/Test_Adj2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+
+//simple Driver program to test Adj_List_Graph class
+
+public class Test_Adj2{
+    
+  public static void main(String[] args)
+  {
+    int nodesG1 = 7;
+    Adj_List_Graph2 G1 = new Adj_List_Graph2(nodesG1 + 1);
+    G1.addEdgeG1(); // adds edges to graph
+    G1.printGraph(); // prints adjacency graph
+    G1.shortestPath(1, nodesG1);  // prints the shortest path distance and number of shortest path of each node
+
+    // Nodes                                  [0, 1, 2, 3, 4, 5, 6, 7]
+    // Distances of each node from 1         [-1, 0, 1, 1, 1, 2, 2, 3]
+    // Shortest path of each node from 1      [0, 1, 1, 1, 1, 2, 1, 3]
+    
+
+    int nodesG2 = 10;
+    Adj_List_Graph2 G2 = new Adj_List_Graph2(nodesG2 + 1);
+    G2.addEdgeG2(); // adds edges to graph
+    G2.printGraph(); // prints adjacency graph
+    G2.shortestPath(1, nodesG2); // prints the shortest path distance and number of shortest path of each node
+    
+
+    // Nodes                                  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // Distances of each node from 1         [-1, 0, 1, 1, 1, 1, 1, 2, 3, 3, 4]
+    // Shortest path of each node from 1      [0, 1, 1, 1, 1, 1, 1, 5, 5, 5, 10]
+    
+  }
+}
